7128c2545faf9c49dff301a34e6cc5677fdfd332
Erdi-H <erdi@umich.edu>
2024-11-17 11:59:18 -0500

    adjusted terminal content and style

--- static/js/home.js
+++ static/js/home.js
@@ -4,9 +4,9 @@ $(function () {
       "stat erdihida<br/>" +
       /*"><span class='caret'>$</span> job: <br/> ^100" +*/
       "><span class='caret'>$</span> programming: C++, C#, Python, Javascript, SQL<br/> ^300" +
-      "><span class='caret'>$</span> frameworks: Python Flask, React.js<br/> ^300" +
+      "><span class='caret'>$</span> frameworks: Python Flask, React.js, Vue.js<br/> ^300" +
       "><span class='caret'>$</span> certifications: CompTIA Security+, CompTIA Network+<br/> ^300" +
-      "><span class='caret'>$</span> hobbies: reading, lifting weights, movies<br/> ^300" +
+      "><span class='caret'>$</span> hobbies: reading, lifting weights, movies, cooking<br/> ^300" +
       "><span class='caret'>$</span> langauges: English, Albanian(beginner), Spanish(beginner)<br/> ^100"
     ],
     showCursor: true,
